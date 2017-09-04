0a595713915b1ffd0de9c26e1ab72251f7bcbac7
Andrei-PC\Andrei <andrei.malyuchik@gmail.com>
2017-09-03 23:30:16 -0600

    Moved images to their own folder and added an authentication class page

---
+++ wksp_includes/auth/user_authentication.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Andrei
+ * Date: 9/1/2017
+ * Time: 5:43 PM
+ */
+
+class user_authentication
+{
+    var $user_first_name;
+    var $user_last_name;
+    var $username;
+    var $user_email;
+    var $user_password;
+
+
+}
